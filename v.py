0e469cfc96a0a02a0d6cde668427a3ff5bea8454
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-11-25 20:44:34 +0330

    Update v.py

--- v.py
+++ v.py
@@ -1,33 +1,21 @@
 import requests
-import socket
-from termcolor import colored  # برای چاپ رنگی
+from termcolor import colored  # for colored printing
 
 def check_http(url):
     try:
         response = requests.get(url)
+        print(f"HTTP Status Code for {url}: {response.status_code}")
         if response.status_code == 200:
             print(colored(f"The website {url} is up and running!", 'green'))
+        elif response.status_code == 404:
+            print(colored(f"Oops! {url} was not found (404 Not Found).", 'red'))
         else:
-            print(colored(f"Looks like {url} is having some issues. Status code: {response.status_code}", 'red'))
-    except requests.RequestException:
-        print(colored(f"Oops! Couldn't reach {url}.", 'red'))
+            print(colored(f"Looks like {url} is having some issues. Status code: {response.status_code}", 'yellow'))
+    except requests.RequestException as e:
+        print(colored(f"An error occurred: {e}", 'red'))
 
-def check_tcp(host, port):
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(5)  # زمان محدودیت اتصال
-        s.connect((host, port))
-        print(colored(f"The TCP connection to {host}:{port} is successful!", 'green'))
-    except socket.error:
-        print(colored(f"Could not connect to {host}:{port}.", 'red'))
-    finally:
-        s.close()
-
-# ورودی‌ها
+# Input
 url_to_check = input("Please enter the URL you want to check: ")
-host_to_check = input("Please enter the host you want to check: ")
-port_to_check = int(input("Please enter the port you want to check: "))
 
-# اجرای توابع چک
+# Perform HTTP check
 check_http(url_to_check)
-check_tcp(host_to_check, port_to_check)
