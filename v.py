c97bc4ffa630bd657a8e16d080e96542c6d04812
Sm3Rr <136627138+Sm3Rr@users.noreply.github.com>
2023-11-25 20:40:32 +0330

    Update v.py

--- v.py
+++ v.py
@@ -1,26 +1,33 @@
-import paramiko
+import requests
+import socket
+from termcolor import colored  # برای چاپ رنگی
 
-def change_vps_password(vps_ip, username, password, new_password):
-    # برقراری ارتباط با VPS
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(vps_ip, username=username, password=password)
+def check_http(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print(colored(f"The website {url} is up and running!", 'green'))
+        else:
+            print(colored(f"Looks like {url} is having some issues. Status code: {response.status_code}", 'red'))
+    except requests.RequestException:
+        print(colored(f"Oops! Couldn't reach {url}.", 'red'))
 
-    # اجرای دستور net user برای تغییر رمز عبور
-    command = f"net user Administrator {new_password}"
-    stdin, stdout, stderr = client.exec_command(command)
-
-    # چاپ خروجی دستور
-    print(stdout.read().decode())
-
-    # بستن اتصال
-    client.close()
+def check_tcp(host, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5)  # زمان محدودیت اتصال
+        s.connect((host, port))
+        print(colored(f"The TCP connection to {host}:{port} is successful!", 'green'))
+    except socket.error:
+        print(colored(f"Could not connect to {host}:{port}.", 'red'))
+    finally:
+        s.close()
 
 # ورودی‌ها
-vps_ip = input("IP VPS را وارد کنید: ")
-username = input("نام کاربری را وارد کنید: ")
-password = input("رمز عبور را وارد کنید: ")
-new_password = input("رمز عبور جدید را وارد کنید: ")
+url_to_check = input("Please enter the URL you want to check: ")
+host_to_check = input("Please enter the host you want to check: ")
+port_to_check = int(input("Please enter the port you want to check: "))
 
-# تغییر رمز عبور سرور مجازی
-change_vps_password(vps_ip, username, password, new_password)
+# اجرای توابع چک
+check_http(url_to_check)
+check_tcp(host_to_check, port_to_check)
